4d6084fda36764b4e04700cc56bb8cfad7da0807
张茂 <zhangmao@boyatriz.com>
2026-07-10 16:38:10 +0800

    持续

---
+++ docs/htmlJsCss/components/CssForBanner.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="title"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const componentName = ref('')
+</script>
+
+<style scoped>
+
+    .container {
+      background-color: lightblue;
+      height: 200px;
+      width: 400px;
+      margin: 0 auto;
+      border: 3px solid;
+    }
+    .title {
+      height: 80px;
+      background-color: orange;
+      box-shadow: 0 2px 15px rgba(50, 50, 50, 0.8);
+      position: relative;
+
+      /* 将 title 拉长 */
+      margin: 30px -30px;
+    }
+    .title::before,
+    .title:after {
+      box-shadow: 0 2px 15px rgba(50, 50, 50, 0.8);
+      content: "";
+      position: absolute;
+      width: 30px;
+      height: 80px;
+      background-color: darkorange;
+
+      transform: skewY(45deg);
+      top: 15px;
+      z-index: -1;
+    }
+    .title::after {
+      right: 0;
+      transform: skewY(-45deg);
+    }
+</style>
---
+++ docs/htmlJsCss/components/CssForCheckbox.vue
@@ -0,0 +1,59 @@
+<template>
+  <label class="checkbox">
+    <input type="checkbox" />
+    <span>同意本协议</span>
+  </label>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    .checkbox {
+      --active-color: #579ef8;
+      position: relative;
+      padding-left: 1.5em;
+      cursor: pointer;
+    }
+    .checkbox input {
+      display: none;
+    }
+    .checkbox span {
+      display: inline-block;
+      user-select: none;
+    }
+    .checkbox span::before,
+    .checkbox span::after {
+      content: "";
+      position: absolute;
+      display: inline-block;
+      width: 1em;
+      height: 1em;
+      position: absolute;
+      left: 0;
+      top: 50%;
+      border-radius: 3px;
+      border: 1px solid;
+      transform: translateY(-50%);
+    }
+    .checkbox span::before {
+      border-color: #ccc;
+    }
+    .checkbox span::after {
+      width: 0.6em;
+      height: 0.6em;
+      left: 0.2em;
+      background-color: var(--active-color);
+      border-color: var(--active-color);
+      opacity: 0;
+    }
+    .checkbox input:checked ~ span {
+      color: var(--active-color);
+    }
+    .checkbox input:checked ~ span::after {
+      opacity: 1;
+    }
+    .checkbox input:checked ~ span::before {
+      border-color: var(--active-color);
+    }
+</style>
